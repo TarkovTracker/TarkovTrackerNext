a34dcf77eb910715cdf3a9aeb053c20b0b8658d4
Thaddeus Bond <thaddeus@thaddeus.io>
2022-06-17 03:33:41 -0400

    Fix Pinia registration

--- tarkov-tracker/src/main.js
+++ tarkov-tracker/src/main.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify'
 import router from './router'
 import { loadFonts } from './plugins/webfontloader'
 import { createI18n } from 'vue-i18n'
+import { createPinia } from 'pinia'
 
 import messages from '@intlify/vite-plugin-vue-i18n/messages'
 
@@ -13,9 +14,12 @@ const i18n = createI18n({
   messages
 })
 
+const pinia = createPinia()
+
 loadFonts()
 
 createApp(App)
+  .use(pinia)
   .use(router)
   .use(vuetify)
   .use(i18n)
