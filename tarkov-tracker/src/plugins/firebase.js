f456a12ab8bdf3b53682a4d796969d3d15a1ed7b
Thaddeus Bond <thaddeus@thaddeus.io>
2022-06-17 18:22:10 -0400

    Add firebase project

---
+++ tarkov-tracker/src/plugins/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzod-ZcUKOmlYNChDAeGWhoatPt6niPu0",
+  authDomain: "tarkovtracker-next.firebaseapp.com",
+  projectId: "tarkovtracker-next",
+  storageBucket: "tarkovtracker-next.appspot.com",
+  messagingSenderId: "260245547337",
+  appId: "1:260245547337:web:e2821d280026b1af539298",
+  measurementId: "G-JYJTN74JNW"
+};
+
+// Initialize Firebase
+const fireapp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(fireapp);
+
+export default {fireapp, analytics}
