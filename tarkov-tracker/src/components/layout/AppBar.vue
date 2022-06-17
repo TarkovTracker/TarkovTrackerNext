cd6989c4f3a96a80ad829f66b5439e616df20d32
Thaddeus Bond <thaddeus@thaddeus.io>
2022-06-17 05:01:33 -0400

    Modify app bar nav icon based upon context

--- tarkov-tracker/src/components/layout/AppBar.vue
+++ tarkov-tracker/src/components/layout/AppBar.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app-bar color="transparent" elevation="0">
     <v-app-bar-nav-icon
+      :icon="navBarIcon"
       variant="text"
       @click.stop="changeNavigationDrawer"
     ></v-app-bar-nav-icon>
@@ -10,19 +11,24 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { useAppStore } from "/src/stores/app.js";
 import { useDisplay } from 'vuetify'
 
 const appStore = useAppStore();
 
-const { mdAndDown } = useDisplay()
+const navBarIcon = computed(() => {
+  return appStore.drawerShow && appStore.drawerRail ? 'mdi-menu-open' : 'mdi-menu'
+})
 
+// Change how the navigation bar is modified based upon the screen size
+// Either change between open/close or rail/full
+const { mdAndDown } = useDisplay()
 function changeNavigationDrawer() {
   if (mdAndDown.value) {
     appStore.drawerShow = !appStore.drawerShow
   }else{
     appStore.drawerRail = !appStore.drawerRail
-    
   }
 }
 </script>
