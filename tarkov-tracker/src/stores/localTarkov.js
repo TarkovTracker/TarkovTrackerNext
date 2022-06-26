2e18a1f5aa2f0967339e268d0cb984df20e7b27c
Thaddeus Bond <thaddeus@thaddeus.io>
2022-06-26 12:19:01 -0400

    Use local storage for localtarkov store

--- tarkov-tracker/src/stores/localTarkov.js
+++ tarkov-tracker/src/stores/localTarkov.js
@@ -1,11 +1,12 @@
 import { defineStore } from 'pinia'
+import { useStorage } from '@vueuse/core'
 
 // The 'localTarkov' Pinia Store. Used to keep tarkov progress state in local storage when not signed in.
 export const useLocalTarkovStore = defineStore('localTarkov', {
   state: () => ({
     // The initial state of the store
     name: 'localTarkov',
-    level: 1,
+    level: useStorage('tarkov_level', 1),
   }),
   getters: {
     // State getters
